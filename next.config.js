a544f7531b247b7e789a98c802adfbe7a4d76945
Polo Bustillo <leopoldobeguiluz1@hotmail.com>
2021-10-04 11:34:22 -0500

    config file update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const path = require("path");
+const nextEnv = require("next-env");
+const dotenvLoad = require("dotenv-load");
+
+const withImages = require("next-images");
+dotenvLoad();
+
+const withNextEnv = nextEnv();
+
+module.exports = withNextEnv(
+  withImages({
+    webpack: (config) => {
+      config.resolve.alias["components"] = path.resolve(
+        __dirname,
+        "components"
+      );
+      config.resolve.alias["public"] = path.resolve(__dirname, "public");
+      return config;
+    },
+  })
+);
