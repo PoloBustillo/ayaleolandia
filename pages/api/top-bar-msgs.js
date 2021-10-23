3cc4779a27a7ea09801c4ab0e5c19d4ddd4fe112
Polo Bustillo <leopoldobeguiluz1@hotmail.com>
2021-10-23 16:34:34 -0500

    logger

--- pages/api/top-bar-msgs.js
+++ pages/api/top-bar-msgs.js
@@ -1,39 +1,53 @@
 /** @format */
 
 import firebaseAdmin from "configs/admin/firebase.js";
+import nc from "next-connect";
+import logger from "middlewares/logger.js";
 
 const storageBucket = firebaseAdmin
   .storage()
   .bucket("gs://ayaleolandia-87c19.appspot.com");
 
-export default async (req, res) => {
-  try {
-    const entries = await firebaseAdmin.firestore().collection("topbar").get();
-    const entriesData = await Promise.all(
-      entries.docs.map(async (entry) => {
-        let data = await entry.data();
-        let imageUrl = null;
+const handler = nc({
+  onError: (err, req, res, next) => {
+    res.status(500).end(err.stack);
+  },
+  onNoMatch: (req, res, next) => {
+    res.status(404).end("Endpoint no se ha habilitado");
+  },
+})
+  .use(logger)
+  .get(async (req, res) => {
+    try {
+      const entries = await firebaseAdmin
+        .firestore()
+        .collection("topbar")
+        .get();
+      const entriesData = await Promise.all(
+        entries.docs.map(async (entry) => {
+          let data = await entry.data();
+          let imageUrl = null;
 
-        if (data.icon_image || data.icon_image != null) {
-          imageUrl = await storageBucket.file(data.icon_image).getSignedUrl({
-            action: "read",
-            expires: "03-17-2025", // this is an arbitrary date
-          });
-          console.log(imageUrl);
-        }
+          if (data.icon_image || data.icon_image != null) {
+            imageUrl = await storageBucket.file(data.icon_image).getSignedUrl({
+              action: "read",
+              expires: "03-17-2025", // this is an arbitrary date
+            });
+          }
 
-        return {
-          text: data.main_msg,
-          subText: data.sub_msg,
-          icon: imageUrl,
-          url: data.url,
-        };
-      })
-    );
+          return {
+            text: data.main_msg,
+            subText: data.sub_msg,
+            icon: imageUrl,
+            url: data.url,
+          };
+        })
+      );
 
-    res.status(200).json(entriesData);
-  } catch (e) {
-    console.log(e);
-    res.status(400).end();
-  }
-};
+      res.status(200).json(entriesData);
+    } catch (e) {
+      res.status(400).end();
+    }
+  });
+
+export default handler;
